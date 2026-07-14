f24de7390793ef4539eec8963a6e0fd05e87231d
orhadad3 <orhadad3@gmail.com>
2026-07-14 10:40:20 +0300

    frontend: initialize React app with Vite

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import "./styles/app.css";
+
+function App() {
+    return (
+        <main className="app">
+            <header className="app-header">
+                <h1>Task Manager</h1>
+                <p>Description...</p>
+            </header>
+        </main>
+    );
+}
+
+export default App;
